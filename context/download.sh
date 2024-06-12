2dcc7274eb688d792623b4e3da1e146419be0849
wanchaozhang <wanchao.zhang@se.com>
2024-06-12 16:53:33 +0800

    update

--- context/download.sh
+++ context/download.sh
@@ -126,4 +126,4 @@ for i in "kubeadm" "kubectl" "kubelet"; do
   sudo curl -L "https://dl.k8s.io/release/${kube_install_version}/bin/linux/arm64/${i}" -o "arm64/bin/${i}"
 done
 
-echo "$(ls -la)"
+echo "$(ls -la amd64/cri)"
